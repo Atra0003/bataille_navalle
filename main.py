e23c524a4f6e1d52516fcbebf3388ca62cb8c2e2
Atra0003 <traoreamara556@gmail.com>
2022-07-23 15:31:32 +0200

    rectification du fichier main

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 
 
 from board import BOARD
-from play_move import PlayMove
-from winner import WINNER
+from boot import BOOT_player
 
-def main():
-    cpt = 0
-    ret_win = None 
-    while cpt < 9 and ret_win == None:
-        if cpt % 2 == 0:
-            print("j1")
-        else:
-            print("j2")
-        PM.placement()
-        if cpt >= 4:
-            ret_win = W.win()
-        if ret_win != None:
-            if ret_win == "X" or ret_win == "x":
-                print("*** victoire joueur 1 ***")
-            else:
-                print("*** victoire joueur 2 ***")
-        cpt += 1
+
 
 
 if __name__ == "__main__":
-    T_BOARD = 3
-    M = BOARD(T_BOARD)
-    M.build_board()
-    M.affiche_board()
-    PM = PlayMove(M)
-    W = WINNER(M)
-    main()
-
+    Board = BOARD()
+    Board.build_board_p1()
+    Board.build_board_p2()
+    Board.affiche_board_p1()
+    
+    B = BOOT_player(Board)
+    
+    boot_1 = B.boot_1()
+    #boot_2 = B.boot_2()
+    #boot_3 = B.boot_3()
+    #boot_4 = B.boot_4()
+    
+    select_boot_1 = B.select_boot(boot_1, 1)
+    #select_boot_2 = B.select_boot(boot_2, 2)
+    #select_boot_3 = B.select_boot(boot_3, 3)
+    #select_boot_4 = B.select_boot(boot_4, 4)
+    
+    print(select_boot_1)
+    
+    
+    
+    
 
 
